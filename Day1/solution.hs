e242b7da157b98ebe671d6da0b1e68c9867657dd
August Matseke <augustma@chalmers.se>
2025-12-04 14:10:41 +0100

    D1P2

--- Day1/solution.hs
+++ Day1/solution.hs
@@ -7,6 +7,7 @@ main = do
   let turns = lines contents
   let dial = 50 :: Int
   putStrLn $ "Part 1: " ++ show (findPass dial turns)
+  putStrLn $ "Part 2: " ++ show (findZPass dial turns)
 
 parseTurn :: String -> Int
 parseTurn [] = 0
@@ -15,10 +16,28 @@ parseTurn (x : xs) = case x of
   'R' -> read xs
 
 findPass :: Int -> [String] -> Int -- dial, turns -> pass
+findPass _ [] = 0
 findPass dial (t : ts) =
   case newDial of
     0 -> 1 + findPass newDial ts
     _ -> findPass newDial ts
   where
     newDial = (dial + parseTurn t) `mod` 100
-findPass dial [] = 0
+
+findZPass :: Int -> [String] -> Int -- dial, turns -> pass
+findZPass _ [] = 0
+findZPass dial (t : ts) =
+  let turn = parseTurn t
+      hits = countZeros dial turn
+      newDial = (dial + turn) `mod` 100
+   in hits + findZPass newDial ts
+
+countZeros :: Int -> Int -> Int
+countZeros _ 0 = 0
+countZeros start turn =
+  if turn > 0
+    then q `div` 100 - p `div` 100
+    else (p - 1) `div` 100 - (q - 1) `div` 100
+  where
+    p = start
+    q = start + turn
